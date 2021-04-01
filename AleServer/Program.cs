55a975e1e627dddbac6bb6c040ff2568d55f065e
coko <cor4lkoala@gmail.com>
2021-04-02 01:12:34 +0900

    add TestClass

--- AleServer/Program.cs
+++ AleServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AleServer.src.Test;
+using System;
 
 namespace AleServer
 {
@@ -6,7 +7,7 @@ namespace AleServer
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      var testClass = new TestClass();
     }
   }
 }
---
+++ AleServer/src/Test/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AleServer.src.Test
+{
+  class TestClass
+  {
+    public TestClass()
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
